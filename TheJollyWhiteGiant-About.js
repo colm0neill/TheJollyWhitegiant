82c22cc531e1eb3449f9af9be9025b47f48f1008
Colm O'Neill <colmoneill89@gmail.com>
2017-05-12 23:01:57 +0100

    Update TheJollyWhiteGiant-About.js

--- TheJollyWhiteGiant-About.js
+++ TheJollyWhiteGiant-About.js
@@ -68,7 +68,7 @@ function removeImg(){
 	
 	if((w == 0)&&(h == 0 )){
 	var main = document.getElementById("a-welcome");
-	main.style.height = "665px";
+	main.style.height = "865px";
 	}
 	else{
 	var main = document.getElementById("a-welcome");
